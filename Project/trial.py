b1778589e4101cd70f617b2024b143ec87ac9446
TBWYO <53593000+TBWYO@users.noreply.github.com>
2023-03-22 11:47:01 -0600

    Fixed the GUI and made it more neat

--- Project/trial.py
+++ Project/trial.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python
-
-#Python -m psychopy.monitors.MonitorCenter
 #Make sure to activate the virtual environment with .\env\scripts\activate
 import os
 import math
@@ -22,32 +20,54 @@ from psychopy import sound
 import psychopy_sounddevice
 
 
-# Set variables for paths we will be using
+# Set variables for paths we will be using, as well as other useful information
 cwd = os.getcwd()
 resourcePath = (cwd + r"\Resources")
 dataPath = (cwd+r"\DataPath")
-hostname = socket.gethostname()
+hostName = socket.gethostname()
+monitor = "testMonitor"
+
+
+def settingsGUI():
+    # Create a dictionary with default values for all the settings
+    default_values = {
+        "Participant": "",
+        "Date": data.getDateStr(),
+        "Computer": hostName,
+        "Com Port": "",
+        "Fullscreen": False,
+        "Bluetooth": False,
+        "Test": False,
+    }
+    # Create the GUI dialog
+    dialog = gui.DlgFromDict(
+        dictionary=default_values,
+        title="Settings",
+        order=["Participant", "Date", "Computer", "Com Port", "Fullscreen", "Bluetooth", "Test"],
+        tip={
+            "Name": "Enter the participant's name",
+            "Com_Port": "Enter the communication port",
+            "Computer": "Enter the computer name",
+            "Date": "Enter the date",
+            "Test": "Enter the test name",
+            "Participant": "Enter the participant ID",
+            "Fullscreen": "Enable/disable fullscreen mode",
+            "Bluetooth": "Enable/disable Bluetooth",
+            "Test?": "Enable/disable test mode",
+        },
+    )
+    if dialog.OK:
+        print("User entered:", default_values)
+        misc.toFile('lastParams.pickle', default_values)
+
+    else:
+        print("User cancelled the dialog")
+
+settingsGUI();
 
 def debug():
     print("Resource Path:", resourcePath)
     print("Data Path:", dataPath)
-    print("Hostname:", hostname)
+    print("hostName:", hostName)
     print("Sound Device:", sound.Sound.__name__)
-
-def settingsUI():
-    monSize = [1920, 1080]
-    info = {}
-    info['FullScreen'] = False
-    info['Test?'] = False
-    info['1. Participant'] = '[PARTICIPANT]'
-    info['Computer'] = hostname
-    info['Date'] = data.getDateStr()
-    info['Bluetooth'] = True
-    info['Com_port'] = 'COM4'
-    dlg = gui.DlgFromDict(info)
-    if dlg.OK:
-        misc.toFile('PST_fMRI_lastParams.pickle', info) 
-    else:
-        core.quit()
-settingsUI();
-debug();
+#debug();
