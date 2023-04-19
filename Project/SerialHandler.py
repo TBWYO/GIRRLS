94a6fb34e3f4a9f65e8b4d025093ae9968c9b765
Michael Lance <notabotatall27@gmail.com>
2023-04-19 15:14:02 -0600

    started python

--- Project/SerialHandler.py
+++ Project/SerialHandler.py
@@ -1,49 +1,20 @@
 import serial
 import time
-#Command Types
-TRANS_TYPE_COMMAND = 0x7E
-TRANS_TYPE_ACKNOWLEDGE = 0x40
-TRANS_TYPE_EVENT = 0x25
 
-#Host Parameters
-ESTABLISH_CONNECTION_SERIAL = 0x53
+ser = serial.Serial('/dev/tyUSB0', 9600)
+ser.flushInput()
+ser.flushOutput()
 
-#Host Commands
-ESTABLISH_CONNECTION = 0x45
-DISPENSE_CANDY = 0x49
-RESET = 0x51
+ser.write(b'~ES')
+ser.flush()
 
-#Client Events
-JAM_OR_EMPTY = 0xa
-DISPENSE_DETECTED = 0x4d
-CANDY_TAKEN = 0x54
+expected_response =b'@es'
+response = b''
 
-#Host Acknowledgements
-CONNECTION_ESTABLISHED = 0x65
-DISPENSING_CANDY = 0x69
-RESETTING = 0x71
+while response != expected_response:
+    if ser.inWaiting() > 0:
+        response += ser.read():
 
-#Client Acknowledgements
-PAUSED_FOR_ASSISTANCE = 0x65
-DISPENSE_NOTED = 0x6d
-CANDY_TAKEN_NOTED = 0x74
+ser.close()
+print(response)
 
-#Hardware Events
-MOTOR_ROTATE = 0x44
-
-establishConnection = [TRANS_TYPE_COMMAND,ESTABLISH_CONNECTION,ESTABLISH_CONNECTION_SERIAL]
-moveMotor = [TRANS_TYPE_COMMAND,DISPENSE_CANDY,MOTOR_ROTATE]
-ser = serial.Serial("/dev/ttyUSB0", 9600, xonxoff = False)
-
-def command_to_send(command, response):
-    ser.reset_input_buffer()
-    ser.write(command)
-    if(ser.read(3) == response):
-        return True;
-    else:
-        return False;
-#print(ser.cts)
-#print(ser.dtr)
-time.sleep(3) # Temporary solution until flow control is implemented
-if command_to_send(establishConnection,b"@es"):
-    command_to_send(moveMotor,b"@iy")
